b961600e82e42e01c65e1e92e348f4d7d3f208d1
michele-pavlenco <michelepavlenco.mp@gmail.com>
2022-05-16 19:48:23 +0200

    added-cards1

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,106 +1,252 @@
 <template>
-<header class="container">
-    <div class="heder-top ">
+  <header>
+    <div class="container">
+      <div class="heder-top">
         <div class="logo">
-<img src="../assets/img/dark-pet-logo.png" alt="">
+          <img src="../assets/img/dark-pet-logo.png" alt="" />
         </div>
         <div class="serchbar">
-            <input type="text" placeholder="Search...">
+          <input type="text" placeholder="Search..." />
         </div>
         <div class="questions">
-            <h4>Questions?</h4>
-            <p>Call us : 1.800.123.4567</p>
+          <h4>Questions?</h4>
+          <p>Call us : 1.800.123.4567</p>
         </div>
         <div class="profile">
-<i class="fa-solid fa-bag-shopping"></i>
-<i class="fa-solid fa-user"></i>
+          <i class="fa-solid fa-bag-shopping"></i>
+          <i class="fa-solid fa-user"></i>
         </div>
+      </div>
+      <nav>
+        <div v-for="(item, index) in navOptions" :key="index">
+          <div class="navoptions">
+            {{ item.name }}
+            <div v-if="item.options">{{ item.options }}</div>
+          </div>
+        </div>
+      </nav>
     </div>
-    <nav class="debug">
-        <div v-for="(name, index) in navOptions" :key="index" >
-           <div class="navoptions" >{{navOptions[1].name}}
-              <div v-if="navOptions.options"> {{navOptions.options}}</div>
-           </div>
-
+    <div class="jumbotron">
+      <div class="jumbotron-top debug container">
+        <span>
+          <i class="fa-solid fa-map"></i>
+          <p>Internetional</p>
+          Shipping Services
+        </span>
+        <span>
+          <i class="fa-solid fa-clock"></i> See our
+          <p>store hours</p>
+          and services
+        </span>
+        <span>
+          <i class="fa-solid fa-check"></i> We price match to give you the
+          <p>best details</p>
+        </span>
+      </div>
+      <div class="jumbotron-img">
+        <div class="jumbotron-text container">
+          <p>Find the best animal supplies</p>
+          <h2>
+            We know animals are a part of your family, let us help take care of
+            them.
+          </h2>
+          <button>Learn more about us</button>
         </div>
-    </nav>
-</header>
+      </div>
+      <div class="jumbotron-bottom container debug">
+        <img src="../assets/img/food-transparent-5.png" alt="pet food" />
+        <div class="jumbotron-bottom-text">
+          <p>Get &euro;25 OFF youre first purchase of our homemade pat foof!</p>
+          <a href="#">Visit the shop</a>
+        </div>
+      </div>
+      <div class="black-jumbotron"></div>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
-    name:'HeaderComponent',
-    data(){ return{
-        navOptions:[
-            {
-                id: 1,
-                name: 'Home',
-                optins:['Home-Alternate']
-            },
-            {
-                 id: 2,
-                name: 'Shop',
-                optins:['Shop no sidebar', 'Shop single sidebar','Shop dual sidebar','Simple product','Variable product']
-            },
-            {
-                 id: 3,
-                name:'About'
-            },
-             {
-                  id: 4,
-                name:'Blog'
-            },
-             {
-                  id: 5,
-                name:'Contact'
-            },
-             {
-                  id: 6,
-                name:'Shop by brand',
-                // options:[]
-            }
-        ]
-}
-}
-}
-
+  name: "HeaderComponent",
+  data() {
+    return {
+      navOptions: [
+        {
+          id: 1,
+          name: "Home",
+          optins: ["Home-Alternate"],
+        },
+        {
+          id: 2,
+          name: "Shop",
+          optins: [
+            "Shop no sidebar",
+            "Shop single sidebar",
+            "Shop dual sidebar",
+            "Simple product",
+            "Variable product",
+          ],
+        },
+        {
+          id: 3,
+          name: "About",
+        },
+        {
+          id: 4,
+          name: "Blog",
+        },
+        {
+          id: 5,
+          name: "Contact",
+        },
+        {
+          id: 6,
+          name: "Shop by brand",
+          // options:[]
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss">
-.heder-top{
-    height: 50px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    border: 1px solid black;
+.heder-top {
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
-.logo img{
-    height: 30px;
+.logo img {
+  height: 30px;
 }
-.serchbar input{
-    background-color:lightgray;
-    padding: 4px;
-    border: 1px solid lightgray;
-    border-radius:30px;
-    width: 200px;
+.serchbar input {
+  background-color: lightgray;
+  padding: 4px;
+  border: 1px solid lightgray;
+  border-radius: 30px;
+  width: 200px;
 }
-.questions{
-    display: flex;
+.questions {
+  display: flex;
 }
-.questions p{
-    color: gray;
+.questions p {
+  color: gray;
 }
-.profile i{
-    margin-left: 10px;
-    
+.profile i {
+  margin-left: 10px;
 }
-nav{
-    height: 50px;
-    display: flex;
-    justify-content: center;
+nav {
+  height: 50px;
+  display: flex;
+  justify-content: center;
 }
-.navoptions{
-    margin: 14px;
+.navoptions {
+  margin: 14px;
+  font-size: 14px;
+}
+.jumbotron {
+  background-image: url(../assets/img/bg-transparent-3.png);
+  background-size: auto;
+  height: calc(100vh - 100px);
+  width: 100%;
+  background-color: #2a382a;
+  color: white;
+  position: relative;
 }
 
+.jumbotron::after {
+  display: block;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  content: "";
+  height: 100%;
+
+  background-color: rgba(0, 0, 0, 0.349);
+  width: 100%;
+}
+.jumbotron-top {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 40px;
+  widows: 100%;
+  font-size: 13px;
+  z-index: 100;
+}
+.jumbotron i {
+  margin: 0px 5px 0px 5px;
+}
+.jumbotron-top span {
+  display: flex;
+  font-weight: 10;
+}
+.jumbotron-top p {
+  font-weight: bold;
+  margin: 0px 4px;
+}
+.jumbotron-img {
+  background-image: url(../assets/img/banner-5-2x-scaled.jpg);
+  width: 100%;
+  height: calc(100vh - 220px);
+  background-size: cover;
+  display: flex;
+  flex-flow: column;
+  flex-wrap: wrap;
+  justify-content: center;
+
+}
+.jumbotron-img p {
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 14px;
+  position: absolute;
+  z-index: 1000;
+  top: 23%;
+  
+}
+.jumbotron-img h2 {
+  width: 40%;
+  font-size: 40px;
+  font-weight: 250;
+  margin: 30px 0px;
+  position: absolute;
+  z-index: 1000;
+  top: 28%;
+}
+.jumbotron-img button {
+  padding: 10px 15px;
+  font-size: 10px;
+  border-radius: 30px;
+  border: 0px;
+  cursor: pointer;
+  position: absolute;
+  z-index: 1000;
+  top: 60%;
+}
+.jumbotron-bottom {
+  display: flex;
+  height: 80px;
+  width: 100%;
+  position: relative;
+}
+.jumbotron-bottom img {
+  height: 60px;
+  position: absolute;
+  bottom: 0;
+  margin-left: 10%;
+  z-index: 100;
+}
+.jumbotron-bottom-text {
+  margin-left: 25%;
+  position: absolute;
+  display: flex;
+  margin-top: 25px;
+  z-index: 100;
+}
+.jumbotron-bottom-text a {
+  color: grey;
+  margin-left: 20px;
+}
 </style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,16 +1,186 @@
 <template>
-    <div>
+  <main>
+    <div class="part1 container">
+      <div class="browse-category">
+        <div class="title">
+          <h2>Browse by category</h2>
+          <p>
+            Augue purus et, tincidunt condimentum mauris. At nibh rutrum mi in.
+            Nisi, vitae interdum eleifend dui, consequat nulla rhoncus dictum.
+            Viverra.
+          </p>
+        </div>
+        <div class="cards1">
+          <div class="card">
+            <img src="../assets/img/product-9.jpg" alt="" />
+            <p>Bed (1)</p>
+          </div>
+          <div class="card">
+            <img src="../assets/img/product-3.jpg" alt="" />
+            <p>Food (6)</p>
+          </div>
+          <div class="card">
+            <img src="../assets/img/product-2.jpg" alt="" />
+            <p>Toys (6)</p>
+          </div>
+          <div class="card">
+            <img src="../assets/img/product-4.jpg" alt="" />
+            <p>Trasport (6)</p>
+          </div>
+        </div>
+        <div class="cards2">
+          <div class="card">
+            <h3>Kibble</h3>
+            <p>Dry dog food</p>
+            <img src="../assets/img/food-transparent-18.png" alt="" />
+            <button>Shop dry food</button>
+          </div>
+          <div class="card">
+            <h3>Moist</h3>
+            <p>Canned dog food</p>
+            <img src="../assets/img/food-transparent-18.png" alt="" />
+            <button>Shop moist food</button>
+          </div>
+          <div class="card">
+            <h3>Frozen</h3>
+            <p>Freeze-Dried dog food</p>
+            <img src="../assets/img/food-transparent-18.png" alt="" />
+            <button>Shop frozen food</button>
+          </div>
+        </div>
+      </div>
+      <div class="new-arrivals">
+          <section class="section1">
+              <p>Find the best animal supplies</p>
+              <h2>New arrivals weekly</h2>
+              <button>Learn more about us</button>
 
+          </section>
+              
+         
+      </div>
     </div>
+  </main>
 </template>
 
 <script>
 export default {
-    name:'MainComponent',
-    
-}
+  name: "MainComponent",
+};
 </script>
 
 <style lang="scss">
+// .part1 {
+//   height: 100vh;
+// }
+.part1 .browse-category {
+  //height: 180vh;
+}
+.part1 .browse-category .title {
+  margin: 80px auto;
+  text-align: center;
+}
+.part1 .browse-category p {
+  margin: 20px 20%;
+  font-weight: 100;
+  font-size: 15px;
+}
+.part1 .browse-category .cards1 {
+  display: flex;
+}
+.part1 .browse-category .card img {
+  width: calc(100% / 1);
+  background-size: contain;
+  padding: 10px;
+}
+.part1 .browse-category .card {
+  text-align: center;
+}
+.part1 .cards2 {
+  display: flex;
+  margin: 80px 0;
+}
+.part1 .cards2 .card {
+ background: #2a382a;
+   background-image:url(../assets/img/bg-transparent-3.png); 
+//background-image: linear-gradient(to bottom, #2a382a ,url(../assets/img/bg-transparent-3.png));
+  //background-color: linear-gradient( 180 #2a382a);
+  margin: 10px;
+  padding: 40px 0;
+  display: flex;
+  justify-content: center;
+  flex-flow: column;
+  align-items: center;
+}
+.part1 .cards2 .card img {
+  width: 60%;
+}
+.part1 .cards2 h3{
+    color: white;
+    font-size: 30px;  
+}
+.part1 .cards2 p{
+    color: #77ab82;
+}
+.part1 .cards2 button{
+    background-color:#77ab82 ;
+    color: white;
+    padding: 10px 15px;
+    border-radius: 30px;
+    border:0px ;
+    font-size: 12px;
+    cursor: pointer;
+}
+.section1{
+    background-image: url(../assets/img/banner-3-2x-scaled.jpg);
+    width: 100%;
+    background-size: cover;
+    height: 400px;
+    display: flex;
+    flex-flow: column wrap;
+    justify-content: space-around;
+    align-items: center;
+    padding: 50px;
+    color: white;
+position: relative;
+z-index: 100;
+}
+.section1 p{
+    text-transform: uppercase;
+    position: absolute;
+    z-index: 100;
+    top: 100px;
+}
+.section1 h2{
+     position: absolute;
+    z-index: 100;
+    top: 160px;
+    font-size: 40px;
+}
+.section1 button{
+    position: absolute;
+    z-index: 100;
+    top: 250px;
+    padding: 10px 15px;
+    border-radius: 30px;
+    font-size: 12px;
+    border:0px;
+    color: gray;
+    cursor: pointer;
+
+
+}
+.section1::after {
+  display: block;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  content: "";
+  height: 100%;
+
+  background-color: rgba(0, 0, 0, 0.349);
+  width: 100%;
+}
+
 
 </style>
